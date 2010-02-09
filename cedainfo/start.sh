6f8dc8a35dee0de2729c25e1ca151f8c7cdd9a3b
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2010-02-09 12:03:53 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@6951 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/start.sh
+++ cedainfo/start.sh
@@ -12,8 +12,15 @@ cd $PROJDIR
 if [ -f $PIDFILE ]; then
     kill `cat -- $PIDFILE`
     rm -f -- $PIDFILE
+    rm -f $SOCKET
 fi
 
 
+umask 007
 /usr/local/bin/python2.5 manage.py runfcgi socket=$SOCKET daemonize=true pidfile=$PIDFILE
+#
+# For some reason umask does not seem to work, so we wait a few seconds then change the permission on the created socket file
+#
+echo "Please wait..."
+sleep 10
 chmod 770 $SOCKET
