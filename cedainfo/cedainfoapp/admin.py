a28152aa40b961ff142185087adeb5fd73c7fc4d
mpritcha <mpritcha@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2010-05-10 16:30:16 +0000

    changed service contact roles to be direct attrs of service (simplifies data entry forms)
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@7384 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/cedainfoapp/admin.py
+++ cedainfo/cedainfoapp/admin.py
@@ -10,20 +10,34 @@ admin.site.register(Role)
 admin.site.register(Person)
 admin.site.register(DataEntityContact)
 admin.site.register(DataEntitySizeMeasurement)
-admin.site.register(Rack)
+#admin.site.register(Rack)
 admin.site.register(Allocation)
 admin.site.register(Service)
-admin.site.register(ServiceContact)
+#admin.site.register(ServiceContact)
 admin.site.register(HostHistory)
 admin.site.register(DataEntityBackupLog)
 admin.site.register(ServiceBackupLog)
 
 # customise the Host admin interface
 class HostAdmin(admin.ModelAdmin):
-    list_display = ('hostname','ip_addr')
-    list_filter = ('supplier','planned_end_of_life', 'retired_on','host_type')
-    ordering = ('hostname','planned_end_of_life',)
+    list_display = ('hostname','ip_addr','host_type','hypervisor','rack')
+    list_filter = ('supplier','planned_end_of_life', 'retired_on','host_type','rack')
+    ordering = ('hostname','planned_end_of_life')
+    # for the "hypervisor" field, restrict the list of available hosts to those with host_type="hypervisor_server"
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "hypervisor":
+	    #kwargs["queryset"] =#Host.objects.filter(host_type="hypervisor_server").order_by('hypervisor') # seems to have no effect
+	    kwargs["queryset"] = Host.objects.filter(host_type="hypervisor_server")
+	    return db_field.formfield(**kwargs)
+	return super(HostAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 admin.site.register(Host, HostAdmin)
+
+# customise the Rack admin interface
+class RackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'room')
+    list_filter = ('room',)
+    ordering = ('name', 'room')
+admin.site.register(Rack, RackAdmin)
 
 # customise the DataEntity admin interface
 class DataEntityAdmin(admin.ModelAdmin):
