50b5337ccff83c13615f8ca59a996f165943660f
mpritcha <mpritcha@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2010-05-20 09:00:43 +0000

    removed user form stuff ...will fix later.
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@7404 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/urls.py
+++ cedainfo/urls.py
@@ -51,8 +51,5 @@ urlpatterns = patterns('',
 
     # userdb
     (r'^userdb/userstats/$', user_stats),
-    (r'^userdb/user/edit/(?P<id>\d+)/$', user_form),
-    (r'^userdb/user/view/(?P<id>\d+)/$', user_view),
-    (r'^userdb/role/emails/(?P<id>\d+)/$', role_emails),
     
 )
