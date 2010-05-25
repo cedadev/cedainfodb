392c083b9b591dcc7ab6979181a05775ae42ba86
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2010-05-25 08:50:30 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@7426 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/start.sh
+++ cedainfo/start.sh
@@ -2,25 +2,5 @@
 #
 # Starts cedainfo Django application. If it is already running then shuts down running instance and restarts.
 #
-# Script is a modified version of the script found at http://docs.djangoproject.com/en/dev/howto/deployment/fastcgi/
-#
-PROJDIR="/home/badc/software/infrastructure/cedainfo/cedainfo"
-PIDFILE="$PROJDIR/cedainfo.pid"
-SOCKET="/var/www/fastcgi/cedadb.sock"
 
-cd $PROJDIR
-if [ -f $PIDFILE ]; then
-    kill `cat -- $PIDFILE`
-    rm -f -- $PIDFILE
-    rm -f $SOCKET
-fi
-
-
-umask 007
-/usr/local/bin/python2.5 manage.py runfcgi socket=$SOCKET daemonize=true pidfile=$PIDFILE
-#
-# For some reason umask does not seem to work, so we wait a few seconds then change the permission on the created socket file
-#
-echo "Please wait..."
-sleep 10
-chmod 770 $SOCKET
+echo "The cedainfodb app should now be started and stopped by the /etc/init.d/cedainfodb script (need to su in order to run)"
