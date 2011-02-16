e748ecb6d3cc048039e081defef9784c4fed280e
mpritcha <mpritcha@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2011-02-16 15:40:23 +0000

    revisions as per meeting on 10/2/2011
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@8365 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/userdb/admin.py
+++ cedainfo/userdb/admin.py
@@ -1,13 +1,12 @@
-from cedainfo.userdb.models import User, Institute, Country, Role, Licence
+from cedainfo.userdb.models import *
 from django.contrib import admin
 
 admin.site.register(Country)
 
-
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('surname', 'othernames', 'accountid', 'emailaddress', 'startdate')
-    search_fields = ['surname', 'othernames', 'accountid', 'emailaddress']
-    list_filter = ('accounttype',)
+    list_display = ('lastname', 'firstname', 'username', 'email', 'startdate')
+    search_fields = ['lastname', 'firstname', 'username', 'email']
+    list_filter = ('field_of_study','startdate','reg_source')
 admin.site.register(User, UserAdmin)
 
 
@@ -18,15 +17,26 @@ class InstituteAdmin(admin.ModelAdmin):
 admin.site.register(Institute, InstituteAdmin)
 
 class RoleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'authtype', 'description', 'emaillistlink')
+    list_display = ('name', 'description', 'emaillistlink')
     search_fields = ['name', 'description']
-    list_filter = ('datacentre',)
+    #list_filter = ('datacentre',)
 admin.site.register(Role, RoleAdmin)
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'valid_from', 'valid_to')
+    search_fields = ['name', 'description']
+    #list_filter = ('valid_from', 'valid_to')
+admin.site.register(Group, GroupAdmin)
+
 class LicenceAdmin(admin.ModelAdmin):
-    list_display = ('role', 'user', 'ver', 'removed')
+    list_display = ('role', 'user', 'group', 'start_date', 'end_date')
     list_filter = ('role',)
 admin.site.register(Licence, LicenceAdmin)
+
+class ConditionsOfUseAdmin(admin.ModelAdmin):
+    list_display = ('role', 'group', 'text')
+    list_filter = ('role', 'group',)
+admin.site.register(ConditionsOfUse, ConditionsOfUseAdmin)
 
 
 
--- cedainfo/userdb/models.py
+++ cedainfo/userdb/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Country(models.Model):
     name = models.CharField(max_length=200)
     area = models.CharField(max_length=50,       
@@ -11,11 +10,8 @@ class Country(models.Model):
                  ("europe","Europe")) )
     isocode =  models.CharField(max_length=200)
 
-
     def __unicode__(self):
         return self.name
-
-
 
 class Institute(models.Model):
     name = models.CharField(max_length=200)
@@ -27,64 +23,73 @@ class Institute(models.Model):
         return self.name
 
 class User(models.Model):
+    userkey = models.AutoField(primary_key=True)
     title = models.CharField(max_length=10,       
-              choices=(("Mr","Mr"),("Mrs","Mrs"),("Dr","Dr"),
-                       ("Miss","Miss"),("Ms","Ms"), ("Prof","Prof"))) 
-    surname = models.CharField(max_length=50) 
-    othernames = models.CharField(max_length=100) 
-    telephoneno = models.CharField(max_length=50, blank=True, null=True) 
-    faxno = models.CharField(max_length=50, blank=True, null=True) 
-    emailaddress = models.EmailField() 
-    comments = models.TextField(blank=True, null=True) 
+              choices=(
+                  ("Mr","Mr"),
+                  ("Mrs","Mrs"),
+                  ("Dr","Dr"),
+                  ("Miss","Miss"),
+                  ("Ms","Ms"),
+                  ("Prof","Prof")
+              )
+            ) 
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)     
+    tel = models.CharField(max_length=50, blank=True, null=True) 
+    address = models.TextField()
+    postcode = models.CharField(max_length=10)
+    webpage = models.CharField(max_length=256, blank=True, null=True)    
+    comments = models.TextField(blank=True, null=True)
     endorsedby = models.CharField(max_length=50, blank=True, null=True) 
-    degree = models.CharField(max_length=20, blank=True, null=True,       
-              choices=(("BS","BS"),("BSc","BSc"),("MA","MA"),
-                       ("MPhil","MPhil"),("MSc","MSc"), ("MSci","MSci"),
-		       ("PhD","PhD"),("RMS","RMS"),("Other","Other"))) 
-    field = models.CharField(max_length=100, blank=True, null=True) 
-    accountid = models.CharField(max_length=50) 
-    accounttype = models.CharField(max_length=30,       
-              choices=(("Tornado","Tornado"),("Web","Web"),("Share","Share"),
-                       ("None","None"))) 
-    encpasswd = models.CharField(max_length=30, blank=True, null=True) 
-    md5passwd = models.CharField(max_length=50, blank=True, null=True) 
-    startdate = models.DateField(auto_now_add=True) 
-    onlinereg = models.BooleanField() 
-    webpage = models.CharField(max_length=256, blank=True, null=True) 
-    sharedetails = models.BooleanField() 
-    datacentre = models.CharField(max_length=50, blank=True, null=True) 
-    openid_username_component = models.CharField(max_length=200, blank=True, null=True) 
-    openid = models.CharField(max_length=256, blank=True, null=True) 
-    department = models.CharField(max_length=256, blank=True, null=True) 
-    address1 = models.CharField(max_length=256, blank=True, null=True) # TODO "address would be fine as textfield
-    address2 = models.CharField(max_length=256, blank=True, null=True) 
-    address3 = models.CharField(max_length=256, blank=True, null=True) 
-    address4 = models.CharField(max_length=256, blank=True, null=True)
-    address5 = models.CharField(max_length=256, blank=True, null=True)
-    institute = models.ForeignKey(Institute)
+    studying_for = models.CharField(max_length=20, blank=True, null=True,
+        choices=(
+            ("BS","BS"),
+            ("BSc","BSc"),
+            ("MA","MA"),
+            ("MPhil","MPhil"),
+            ("MSc","MSc"),
+            ("MSci","MSci"),
+            ("PhD","PhD"),
+            ("RMS","RMS"),
+            ("Other","Other")
+        )
+    ) 
+    field_of_study = models.CharField(max_length=100, blank=True, null=True) # (choices : as per current table)
+    supervisor = models.CharField(max_length=50, blank=True, null=True)
+    password_md5 = models.CharField(max_length=50, blank=True, null=True)# md5 encrypted password
+    startdate = models.DateField(auto_now_add=True) # date account created
+    reg_source = models.CharField(max_length=50, blank=True, null=True) # where the user registered (choices : as per current table)
+    openid = models.CharField(max_length=256, blank=True, null=True)    
+    username = models.CharField(max_length=50)
     # TODO custom save method:
     #   - if emailaddress or institute are changned, notify helpdesk
 
     def __unicode__(self):
-        return "%s %s %s" % (self.title, self.othernames, self.surname)
-    
+        return "%s %s %s" % (self.title, self.firstname, self.lastname)
 
-class Role(models.Model):
-    name = models.CharField(max_length=50) 
-    authtype = models.CharField(max_length=50)
-    grouptype = models.CharField(max_length=50)
+class Group(models.Model):
+    # use implicit id as PK
+    name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
-    source = models.CharField(max_length=50)
-    ukmoform = models.BooleanField()
-    comments = models.TextField()
-    directory = models.CharField(max_length=200)
-    metdata = models.BooleanField()
-    conditions = models.CharField(max_length=200)
-    defaultreglength = models.IntegerField()
-    datacentre = models.CharField(max_length=50) 
-    infourl = models.CharField(max_length=200)
-    authorisers = models.ManyToManyField(User, blank=True)
-    #authviewers = models.ManyToManyField(
+    valid_from = models.DateField() # Default? Allow Blank/Null?
+    valid_to = models.DateField() # Default? Allow Blank/Null?
+    def __unicode__(self):
+        return self.name
+        
+class Role(models.Model):
+    # use implicit id as PK
+    name = models.CharField(max_length=50) 
+    description = models.TextField()
+    
+    # Suggested entries:
+    # get (can read data)
+    # put (can deposit ...not write!) data
+    # admin (can grant/revoke any role to any other User in that Group)
+    # receiveadminnotification 
+    # viewmembership (can view membership of a Group)
+
     def __unicode__(self):
         return self.name
 
@@ -92,26 +97,76 @@ class Role(models.Model):
         return '''<a href = "/userdb/role/emails/%s">Emails</a> 
 	          <a href = "/admin/userdb/user/?licence__role=%s&licence__removed=0">Users</a>''' %  (self.id, self.id)
     emaillistlink.allow_tags = True
-
-
-
-class Licence(models.Model):
-    role = models.ForeignKey(Role)
-    user = models.ForeignKey(User)
-    ver = models.IntegerField()
-    endorsedby = models.CharField(max_length=50) 
-    endorseddate = models.DateField(auto_now_add=True)
-    research = models.TextField() 
-    nercfunded = models.BooleanField()
-    removed = models.BooleanField()
-    removeddate = models.DateField(null=True)
-    grantref = models.CharField(max_length=50, blank=True, null=True) 
-    openpub = models.CharField(max_length=10, blank=True, null=True) 
-    extrainfo = models.TextField(blank=True, null=True) 
-    expiredate = models.DateField(null=True)
     
+ 
+
+class ConditionsOfUse(models.Model):
+    # use implicit id as PK
+    # ? name?
+    role = models.ForeignKey(Role)
+    group = models.ForeignKey(Group)
+    text = models.TextField() # ?
     def __unicode__(self):
-        return "%s - %s" % (self.role, self.user.surname)
+        return "%s - %s" % (self.role, self.group)    #?
+    
+class Licence(models.Model):
+    # use implicit id as PK
+    user = models.ForeignKey(User)
+    institute = models.ForeignKey(Institute)
+    role = models.ForeignKey(Role, related_name='licence_role')    
+    group = models.ForeignKey(Role, related_name='licence_group')
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField() # default = now + N years?
+    research = models.TextField()     
+    grantref = models.CharField(max_length=50, blank=True, null=True)    
+    conditions = models.ForeignKey(ConditionsOfUse)
+    def __unicode__(self):
+        return "%s - %s" % (self.role, self.user.lastname)
+
+# ditched fields
+
+    # ditched from User
+    #accounttype = models.CharField(max_length=30,       
+    #          choices=(("Tornado","Tornado"),("Web","Web"),("Share","Share"),
+    #                   ("None","None"))) 
+    #encpasswd = models.CharField(max_length=30, blank=True, null=True) 
+    #onlinereg = models.BooleanField() 
+    #sharedetails = models.BooleanField() 
+    #openid_username_component = models.CharField(max_length=200, blank=True, null=True) 
+    #department = models.CharField(max_length=256, blank=True, null=True) 
+    #address1 = models.CharField(max_length=256, blank=True, null=True) # TODO "address would be fine as textfield
+    #address2 = models.CharField(max_length=256, blank=True, null=True) 
+    #address3 = models.CharField(max_length=256, blank=True, null=True) 
+    #address4 = models.CharField(max_length=256, blank=True, null=True)
+    #address5 = models.CharField(max_length=256, blank=True, null=True)
+    #institute = models.ForeignKey(Institute)
+
+    # ditched from Role
+    #authtype = models.CharField(max_length=50)
+    #grouptype = models.CharField(max_length=50)
+    #source = models.CharField(max_length=50)
+    #ukmoform = models.BooleanField()
+    #comments = models.TextField()
+    #directory = models.CharField(max_length=200)
+    #metdata = models.BooleanField()
+    #conditions = models.CharField(max_length=200)
+    #defaultreglength = models.IntegerField()
+    #datacentre = models.CharField(max_length=50) 
+    #infourl = models.CharField(max_length=200)
+    #authorisers = models.ManyToManyField(User, blank=True)
+    #authviewers = models.ManyToManyField(  
+    
+    # ditched from licence
+    #ver = models.IntegerField()
+    #endorsedby = models.CharField(max_length=50) 
+    #nercfunded = models.BooleanField()
+    #removed = models.BooleanField()
+    #removeddate = models.DateField(null=True)
+    #openpub = models.CharField(max_length=10, blank=True, null=True) 
+    #extrainfo = models.TextField(blank=True, null=True) 
+    #expiredate = models.DateField(null=True)
+    
+
     
     
     
