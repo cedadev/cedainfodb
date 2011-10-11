a8ba6fbbf2a9e1465b027c62a93d6b1cda3aa6ef
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2011-10-11 13:42:35 +0000

    Updated for latest version of django
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@8992 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/urls.py
+++ cedainfo/urls.py
@@ -23,7 +23,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.MEDIA_ROOT,'show_indexes': True}),
     # views
     # home page
