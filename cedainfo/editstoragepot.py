3e8b3eb134a100de0b15fb1d5bb46acde8479c48
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2011-10-11 15:22:49 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@8999 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/editstoragepot.py
@@ -0,0 +1,29 @@
+# script to add storage pot for manual fileset set up.
+# Sam Pepler 2011-09-22
+
+import getopt, sys
+import os, errno
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from cedainfoapp.models import *
+
+
+if __name__=="__main__":
+
+    logical_path = sys.argv[1]
+    filesets = FileSet.objects.filter(logical_path = logical_path)
+    fs = filesets[0] 
+    print "found fileset %s" % fs
+    if fs.storage_pot == None or fs.storage_pot == '':
+        print "fileset has no storage pot set"
+        fs.storage_pot = "archive/spot-%s" % fs.id
+        fs.save()         
+    else: 
+        fs.storage_pot = "archive/spot-%s" % fs.id
+        print "fileset already has storage pot set"
+
+
+
