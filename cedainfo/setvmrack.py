1a42a71e499619f2411e198fc5fcef4f7fb9609d
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2011-11-03 11:24:54 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@9060 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/setvmrack.py
@@ -0,0 +1,20 @@
+# script to add storage pot for manual fileset set up.
+# Sam Pepler 2011-09-22
+
+import getopt, sys
+import os, errno
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from cedainfoapp.models import *
+
+
+if __name__=="__main__":
+
+    vhosts = Host.objects.filter(host_type = "virtual_server")
+    for vh in vhosts:
+        print "Virtual host: %s (%s)  %s" % (vh, vh.hypervisor, vh.retired_on)
+        
+ 
