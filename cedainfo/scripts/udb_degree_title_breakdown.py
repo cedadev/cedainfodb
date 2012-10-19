d51479624f7283834fce82a55510b317eb1f8e3a
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2012-10-19 11:40:00 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@10522 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/scripts/udb_degree_title_breakdown.py
@@ -0,0 +1,25 @@
+#
+# Generates breakdown of users 'Title' by 'Degree type' for 'active' users.
+# 'Active' users have at least one current dataset. The output from this program
+# can be piped through sort and then "uniq -c" to generate the final stats.
+#
+# This script is intended to be run via the 'runscript' option of manage.py:
+#
+#    python manage.py runscript udb_degree_title_breakdown
+#
+  
+from udbadmin.models import *
+
+def run():
+   
+   users = User.objects.all()
+   
+   for user in users:
+      if user.datasetCount() > 0: 
+       
+         if not user.degree: 
+            degree = 'None'
+         else:
+            degree = user.degree
+               
+         print "%s:%s" % (degree, user.title)
