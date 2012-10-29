0ff8eca5c5b634bbdc7b272d50f8ffffca3ab16f
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2012-10-29 09:18:15 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@10571 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/scripts/jasmin_group_members.sh
+++ cedainfo/scripts/jasmin_group_members.sh
@@ -1,5 +1,11 @@
 #
-# Use ldapsearch to list members of given group
+# Use ldapsearch to list members of given group. If no group is specified then list all members of all groups
 #
-echo "Group: $1"
-ldapsearch -LL -x -H ldap://homer.esc.rl.ac.uk -b "cn=$1,ou=ceda,ou=Groups,o=hpc,dc=rl,dc=ac,dc=uk"  |grep 'memberUid:'|cut -d' ' -f2|sort
+
+if [ $# -ne 1 ]
+then
+   ldapsearch -LLL -x -H ldap://homer.esc.rl.ac.uk -b "ou=ceda,ou=Groups,o=hpc,dc=rl,dc=ac,dc=uk" dn memberUid
+else
+   echo "Group: $1"
+   ldapsearch -LL -x -H ldap://homer.esc.rl.ac.uk -b "cn=$1,ou=ceda,ou=Groups,o=hpc,dc=rl,dc=ac,dc=uk"  |grep 'memberUid:'|cut -d' ' -f2|sort
+fi
