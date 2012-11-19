9eb754cd77352c37793d165734ccb6c4b1fbb0a6
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2012-11-19 16:17:08 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@10713 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/partition_check_and_remove.py
+++ cedainfo/partition_check_and_remove.py
@@ -38,24 +38,92 @@ class TidyRun:
             self.state = {'files_deleted':0, 'files_checked':0, 'dir_deleted':0, 'vol_deleted':0, 
                      'filesets_checked':0, 'partitions_checked':0}
             self.LOG = open("%s.log" % filename, 'a')
-            self.state['current_partition'] = None
+            self.state['current_partition_index'] = 0
             self.state['current_fileset']=None
             self.state['run_start'] = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+            self.LOG.write("==== started: %s\n" % self.state['run_start'])
             self.state['partitions_todo'] = []
             for p in Partition.objects.filter(status='Migrating').order_by('mountpoint'): 
                 self.state['partitions_todo'].append(p.pk)
+            self.partition = Partition.objects.get(pk=self.state['partitions_todo'][self.state['current_partition_index']])
             self.write_runfile()
             
         else: 
             f = open(filename)
             self.state = pickle.load(f)
             self.LOG = open("%s.log" % filename, 'a')
+            self.state['reload'] =  time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+            self.partition = Partition.objects.get(pk=self.state['partitions_todo'][self.state['current_partition_index']])            
+            self.write_runfile()
             
     def write_runfile(self):
         output = open(self.filename, 'wb')
         pickle.dump(self.state, output)
         output.close()
 
+    def next_partition(self):
+        self.partition = Partition.objects.get(pk=self.state['partitions_todo'][self.state['current_partition_index']])
+        self.state['current_partition_index'] = self.state['current_partition_index'] + 1
+        if self.state['current_partition_index'] >= len(self.state['partitions_todo']): 
+            raise "No more partitions - STOP."
+        self.partition = Partition.objects.get(pk=self.state['partitions_todo'][self.state['current_partition_index']])
+        self.write_runfile()
+        
+    def check_partition(self):
+        partition  = self.partition
+        old_archivedir = os.path.join(partition.mountpoint,'archive')
+        spots = os.listdir(old_archivedir)
+
+        print spots
+
+        for s in spots:
+            filesets = FileSet.objects.filter(storage_pot=s, storage_pot_type = 'archive')
+            old_spotpath = os.path.join( old_archivedir, s)
+            if len(filesets)==0: 
+                self.LOG.write("** %s no matching fileset- skip\n" %s)
+                continue
+            if len(filesets)>1:
+                self.LOG.write("** %s more than 1 matching fileset- skip\n" %s)
+                continue
+            print filesets
+            fileset = filesets[0]
+            new_spotpath = fileset. storage_path()
+            self.LOG.write("old path %s was migrated to %s\n" % (old_spotpath, new_spotpath))
+            if old_spotpath == new_spotpath:
+                self.LOG.write("** %s old and new the same - skip\n" %s)
+                continue
+
+            # latest size measurements
+            size = fileset.last_size()
+            self.LOG.write("%s\n" %size)
+        
+
+            # storage-d backup size
+            backup= fileset.backup_summary()
+            self.LOG.write("%s\n" %backup)
+
+            # crude storage-d size finder via screen scrape... 
+            #url = 'http://storaged-monitor.esc.rl.ac.uk/storaged_ceda/CEDA_Fileset_Summary.php?%s' % s
+            #import urllib2
+            #f = urllib2.urlopen(url)
+            #for i in range(7): f.readline()
+            #backup = f.readline()
+            #backup = backup.split('<td')
+            #m = re.search('>(\d*)</a></td>', backup[3])
+            #backupfiles = m.group(1)
+            #if backupfiles !='': backupfiles = int(backupfiles)
+            #else: backupfiles = 0 
+            #print backupfiles, size.no_files
+
+            # skip if no backup         
+            #if backupfiles == size.no_files: print "**** exactly the right size backup ****"
+            #elif backupfiles > 0: print "++++ some backup done ++++"
+            #else: 
+            #    print "no backup done - skip"
+            #    continue 
+
+            #check_del(old_spotpath, new_spotpath)
+        
 
 
 def check_del(old, new):
@@ -91,64 +159,10 @@ if __name__=="__main__":
 
     T.write_runfile()
 
-    partition = sys.argv[1]
-    
-    print partition
-    
-    partition = Partition.objects.filter(mountpoint=partition)[0]
-    
-    print partition
-    old_archivedir = os.path.join(partition.mountpoint,'archive')
-    spots = os.listdir(old_archivedir)
+   
+    T.check_partition()
 
-    print spots
-    
-    for s in spots:
-        filesets = FileSet.objects.filter(storage_pot=s, storage_pot_type = 'archive')
-        old_spotpath = os.path.join( old_archivedir, s)
-        if len(filesets)==0: 
-            print "** %s no matching fileset- skip" %s
-            continue
-        if len(filesets)>1:
-            print "** %s more than 1 matching fileset- skip" %s
-            continue
-        print filesets
-        fileset = filesets[0]
-        new_spotpath = fileset. storage_path()
-        print "old path %s was migrated to %s" % (old_spotpath, new_spotpath)
-        if old_spotpath == new_spotpat:
-            print "** %s old and new the same - skip" %s
-            continue
 
-        # latest size measurements
-        size = fileset.last_size()
-        print size
-        
-
-        # crude storage-d size finder via screen scrape... 
-        url = 'http://storaged-monitor.esc.rl.ac.uk/storaged_ceda/CEDA_Fileset_Summary.php?%s' % s
-        import urllib2
-        f = urllib2.urlopen(url)
-        for i in range(7): f.readline()
-        backup = f.readline()
-        backup = backup.split('<td')
-        m = re.search('>(\d*)</a></td>', backup[3])
-        backupfiles = m.group(1)
-        if backupfiles !='': backupfiles = int(backupfiles)
-        else: backupfiles = 0 
-        print backupfiles, size.no_files
-
-        # skip if no backup         
-        if backupfiles == size.no_files: print "**** exactly the right size backup ****"
-        elif backupfiles > 0: print "++++ some backup done ++++"
-        else: 
-            print "no backup done - skip"
-            continue 
-
-        check_del(old_spotpath, new_spotpath)
-        
-
-    #from_partition.status = 'Migrating'
 
 	
 	 
