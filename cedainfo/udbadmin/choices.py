93c878d74fa470f171ff9af16a6b09a348cb8828
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-02-28 09:04:28 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@11525 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/udbadmin/choices.py
+++ cedainfo/udbadmin/choices.py
@@ -1,11 +1,43 @@
+'''
+BSD Licence
+Copyright (c) 2012, Science & Technology Facilities Council (STFC)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, 
+        this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+    * Neither the name of the Science & Technology Facilities Council (STFC) 
+        nor the names of its contributors may be used to endorse or promote 
+        products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Created on 28 Nov 2012
+
+@author: Andrew Harwood
+@author: Maurizio Nagni
+'''
 DATASET_REGISTRATION_TYPES=(
 
    ("manual","manual"),
    ("online","online"),
    ("public","public"),
    ("signature", "signature"),
-   ("badc", "badc"),
-   ("none", "none")
+   ("badc", "badc")
 )
 
 DATACENTRES=(
@@ -30,6 +62,15 @@ FUNDING_TYPES=(
    ("Other", "Other")
 )
 
+INSTITUTE_TYPES=(
+   ("Commercial", "Commercial"),
+   ("Government", "Government"),
+   ("NERC", "NERC"),
+   ("School", "School"),
+   ("University", "University"),
+   ("Other", "Other")
+)
+
 NERC_FUNDED=(
    (0, "No"),
    (-1, "Yes")
@@ -40,5 +81,3 @@ OPEN_PUBLICATION=(
    ("n", "No"),
    ("y", "Yes")
 )
-
-
