4eff41cae93ca77ce99f14f3907ff88e55edb50d
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-03-08 16:40:53 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@11594 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/do_next_audit.py
+++ cedainfo/do_next_audit.py
@@ -13,7 +13,8 @@ from cedainfoapp.models import FileSet, Partition, Audit
 
 def usage():
     print "Cron script to du audit next fileset"
-    print "Usage: %s" % sys.argv[0]
+    print "Usage: %s <N>" % sys.argv[0]
+    print " N= number of audits to do"
     print
 
 
@@ -22,13 +23,20 @@ def pick_audit():
     # pick an audit to do: 
     # 1) any fileset that has no privious audit
     # 2) any fileset that has 
-    filesets = FileSet.objects.filter(storage_pot_type='archive')
+    filesets = FileSet.objects.filter(storage_pot_type='archive', storage_pot__isnull=False)
     fileset_to_audit = None
     oldest_audit = datetime.datetime.now()
     for f in filesets:
         started_last_audit = f.last_audit('started')
         finished_last_audit = f.last_audit('analysed') 
-    
+        error_last_audit = f.last_audit('error') 
+
+        # skip if last audit got an error
+        if error_last_audit != None:    
+            if finished_last_audit == None or error_last_audit.starttime > finished_last_audit.starttime:  
+                print "Ignore - audit got an error" 
+                continue
+
         # if started audit but then skip this file set
 	if started_last_audit != None:
             print " --- Already started" 
