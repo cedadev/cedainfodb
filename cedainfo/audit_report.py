c2678e4d5c407018fb1f2e0344818aff99da64fd
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-06-03 14:42:13 +0000

    added an audit report tool to list corrupt files
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@12219 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/audit_report.py
@@ -0,0 +1,26 @@
+# script to add storage pot for manual fileset set up.
+# Sam Pepler 2011-09-22
+
+import getopt, sys
+import os, errno
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from cedainfoapp.models import *
+
+
+if __name__=="__main__":
+
+    audit_id = int(sys.argv[1])
+    audit = Audit.objects.filter(pk=audit_id)
+    audit = audit[0]
+
+    # find the last finished audit of this fileset for comparison
+    prev_audit = Audit.objects.filter(fileset=audit.fileset, auditstate='analysed').order_by('-starttime')[0]
+
+    result = audit.compare(prev_audit)
+    print "Audit of %s\n============\nReported corruption of the following files\n" % audit.fileset
+    for line in result['corrupt']:
+        print line,
