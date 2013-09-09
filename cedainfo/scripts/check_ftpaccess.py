8f0b98566d5894a7f7aa96f10235eab8d7c171c4
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-09-09 08:46:09 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@12925 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/scripts/check_ftpaccess.py
@@ -0,0 +1,39 @@
+#
+# Report on any storage directory that does not contain an ftpaccess
+# file. The ftp server needs a file in each linked directory in order
+# to allow access to files within that directory. 
+#
+import os
+import sys
+
+import psycopg2
+
+
+connection = psycopg2.connect(dbname="cedainfo", 
+                                host="bora.badc.rl.ac.uk",
+                                user="cedainfo", 
+                                password="ler239b")
+cursor = connection.cursor()
+
+sql = """
+select logical_path, mountpoint, storage_pot  from cedainfoapp_fileset, cedainfoapp_partition
+where cedainfoapp_fileset.partition_id = cedainfoapp_partition.id
+and logical_path like '/badc/%'
+order by mountpoint"""
+
+cursor.execute(sql)
+
+recs = cursor.fetchall()
+
+for rec in recs:
+   
+   logical_path = rec[0]
+   directory = rec[1] + '/archive/' + rec[2]
+   
+   if not os.path.exists(directory + '/.ftpaccess'):
+       print logical_path, directory 
+
+
+
+    
+ 
