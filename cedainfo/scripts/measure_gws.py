39c0e4e33307b5d659912836661fe45067ecc371
mpritcha <mpritcha@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-09-23 15:50:59 +0000

    not finished yet
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@13028 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/scripts/measure_gws.py
+++ cedainfo/scripts/measure_gws.py
@@ -1,11 +1,21 @@
 #
 # Script to measure USED sizes of Group Workspaces
 from cedainfoapp.models import GWS
+import os
 
 def measure(gws):
-    print "Measuring %s" % gws.name
+	dir = os.path.join(gws.path, gws.name)
+	if os.path.isdir(dir):
+		print "Measuring %s at %s" % (gws.name, dir)
+        
+	else:
+		raise Exception("Directory does not exist: %s" % dir)
+	
     
 
 def run():
     for i in GWS.objects.all():
-        measure(i)
+        try:
+			measure(i)
+        except Exception, e:
+            print "Error measuring GWS",str(e)
