9390e81fb155e81c16949bee9fdd7e264871fa84
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-10-02 13:59:51 +0000

    removed proginfo app
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@13093 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/urls.py
+++ cedainfo/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic import list_detail
 from django.conf import settings
-from proginfo.views import *
-from proginfo.models import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -28,27 +26,7 @@ urlpatterns = patterns('',
     (r'^accounts/login/$', 'django.contrib.auth.views.login'), 
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'), 
 
-    
-    # scisup proginfo tool
-    (r'^programme/$', index),
-    (r'^programme/(?P<prog_id>\d+)/add_proj$', add_proj),
-    (r'^programme/(?P<prog_id>\d+)/DMP$', prog_DMP),
-    (r'^programme/(?P<prog_id>\d+)/cost$', prog_cost),
-    (r'^programme/(?P<prog_id>\d+)/add_prog_note$', add_prog_note),
-    (r'^programme/(?P<prog_id>\d+)/help$', prog_help),
-    (r'^project/(?P<proj_id>\d+)/add_proj_note$', add_proj_note),
-    (r'^project/(?P<proj_id>\d+)/add_data$', add_data),
-    (r'^project/(?P<proj_id>\d+)/DMP$', proj_DMP),
-    (r'^project/(?P<proj_id>\d+)/cost$', proj_cost),
-    (r'^project/(?P<proj_id>\d+)/summary$', proj_summary),
-    (r'^project/(?P<proj_id>\d+)/help$', proj_help),
-    (r'^dataproduct/(?P<data_id>\d+)/add_data_note$', add_data_note),
-    (r'^dataproduct/(?P<data_id>\d+)/help$', data_help),
-
-    (r'^newprojects$', newprojects),
-    (r'^newproj$', add_proj_gotw),
-
-    (r'^udbadmin/', include('cedainfo.udbadmin.urls')),   
+   # (r'^udbadmin/', include('cedainfo.udbadmin.urls')),   
 
     (r'^dmp/', include('cedainfo.dmp.urls')),   
 )
