7565eccc76b6c6b6037f777f27e679cdc59b8221
mpritcha <mpritcha@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-10-22 16:04:22 +0000

    Added filtering by jasmin-login, cems-login. Filters got too messy so used Q.
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@13250 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/scripts/udb_dn_accountid.py
+++ cedainfo/scripts/udb_dn_accountid.py
@@ -4,6 +4,8 @@
 #    python manage.py runscript <script-name-without-py-extension>
 #
 import sys
+
+from django.db.models import Q
   
 from udbadmin.models import *
 
@@ -14,5 +16,29 @@ DN_PREFIX = "/DC=uk/DC=ac/DC=ceda/O=STFC RAL/CN="
      
 def run():
 
-    for user in User.objects.filter(pk__gt=0).filter(openid__isnull=False).exclude(openid="").exclude(accountid=""):
+    udjs = Datasetjoin.objects.filter(
+        Q(datasetid="jasmin-login") | Q(datasetid="cems-login"),
+        Q(userkey__gt=0),
+        Q(removed=0)
+    
+    accountids = []
+    
+    for udj in udjs:
+       accountids.append(udj.userkey.accountid)
+       
+    
+    
+    users = User.objects.filter(
+        Q(accountid__in=accountids),
+        Q(pk__gt=0),
+        Q(openid__isnull=False),
+        Q(accountid__isnull=False)
+    ).exclude(openid="").exclude(accountid="")
+    
+    for user in users:
         print "%s%s %s" % (DN_PREFIX, user.openid, user.accountid)
+
+
+        
+#    for user in User.objects.filter(pk__gt=0).filter(openid__isnull=False).filter(accountid__isnull=False).exclude(openid="").exclude(accountid=""):
+#        print "%s%s %s" % (DN_PREFIX, user.openid, user.accountid)
