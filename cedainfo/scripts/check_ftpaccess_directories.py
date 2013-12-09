fedcafbfdbae1f7f175e9a11a6b90b9c3a64561a
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2013-12-09 17:07:03 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@13524 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/scripts/check_ftpaccess_directories.py
@@ -0,0 +1,23 @@
+import os
+
+links_file = open ('links.lis', 'r')
+
+for line in links_file:
+    link = line.rstrip()
+    
+    if not os.path.islink(link):
+        print 'ERROR: %s is not a link' % link
+	continue
+	
+    target = os.readlink(link)
+    
+    if target.startswith('/datacentre/'):
+#        print link, target
+
+        
+	if not os.path.exists(target + '/.ftpaccess'):
+	    print 'Missing ftpaccess file in : %s ' % target	
+	
+    
+    	
+	
