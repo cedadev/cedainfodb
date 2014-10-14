0862124b171e8286d1fbc25236e8eed6c8c39d0c
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2014-10-14 11:47:43 +0000

    Added script to do df.
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@15357 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/scripts/df.py
@@ -0,0 +1,17 @@
+# script to update all df info for partitions
+
+
+from cedainfoapp.models import *
+
+def do_dfs():
+    # do df for all partitions
+    partitions = Partition.objects.all()
+    for p in partitions:
+        print "Doing df of partition: %s " % p
+        p.df()  
+
+
+def run ():
+    do_dfs()
+
+    
