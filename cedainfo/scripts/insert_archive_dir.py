c5b4735a77cb6853b43c642fcf1588ce66f76859
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2015-12-03 14:12:18 +0000

    Added a insert dir script
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@43977 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/scripts/insert_archive_dir.py
+++ cedainfo/scripts/insert_archive_dir.py
@@ -9,28 +9,28 @@ from cedainfoapp.models import *
 
 
 def run (*args):
+    """Insert a directory at the given path. The directory will have the content of the original given path. """
     archive_path = args[0]
     archive_path = archive_path.rstrip('/')
     archive_parent_dir = os.path.dirname(archive_path)
-    change_to = args[1]
+    insert_dir = args[1]
 
-    print archive_path, change_to
+    print archive_path, insert_dir
 
     if not os.path.isdir(archive_path):
         raise Exception("Need a existing directory in archive")
 
-    if "/" in change_to:
+    if "/" in insert_dir:
         raise Exception("Can't have slashes in destination argument.")
 
-    filesets_to_change = FileSet.objects.filter(logical_path__startswith=archive_path)
+    filesets_to_change = FileSet.objects.filter(logical_path__startswith=archive_path+"/")
     print filesets_to_change
 
     # new path
-    new_path = os.path.join(archive_parent_dir, change_to)
+    new_path = os.path.join(archive_path, insert_dir)
 
-    # rename dir
-    print "rename %s -> %s" % (archive_path, new_path)
-    # os.rename(archive_path, new_path)
+    # make dir
+    # os.mkdir(new_path, mode=0o755)
 
     for fs in filesets_to_change:
         print fs.logical_path
