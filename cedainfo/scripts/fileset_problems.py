efee65d49b850a2963f37f2e1700be5ac3cf076e
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2016-05-11 09:49:45 +0000

    Updated limits on warnings.
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@45127 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/scripts/fileset_problems.py
+++ cedainfo/scripts/fileset_problems.py
@@ -26,12 +26,12 @@ def partition_problems():
     partitions = Partition.objects.exclude(status="Retired")
     # list overfilled partitions
     for p in partitions:
-        if 100.0* p.used_bytes/(p.capacity_bytes+1) > 98.0: 
+        if 100.0 * p.used_bytes/(p.capacity_bytes+1) > 99.0:
             print_msg(p, "Partition over filled")
     # list overallocated partitions
     for p in partitions:
         allocated = p.allocated() + p.secondary_allocated()
-        if 100.0* allocated/(p.capacity_bytes+1) > 87.0:
+        if 100.0 * allocated/(p.capacity_bytes+1) > 87.0:
             print_msg(p, "Partition overallocated")
 
 def fileset_problems():
@@ -48,36 +48,46 @@ def fileset_problems():
         if not first_time:
             last_fssm = fssms[0]
 
-            too_many_files =  last_fssm.no_files > 5000000
-            too_big = f.overall_final_size > 30000000000000
+            too_many_files = last_fssm.no_files > 5000000
+            too_big = f.overall_final_size > 40000000000000
             over_alloc = last_fssm.size > f.overall_final_size
 
-            changing = False # assume static and look for changes
-            npoints=0
+            changing = False     # assume static and look for changes
+            npoints = 0
             for fssm in fssms[1:]:                    # loop over fileset measurements from second to last...
-                if datetime.now() - fssm.date > timedelta(days=120): break  # only look in past 120 days
-                npoints +=1         # number of points within 120 days
+                if datetime.now() - fssm.date > timedelta(days=120):
+                    break  # only look in past 120 days
+                npoints += 1         # number of points within 120 days
                 if fssm.size != last_fssm.size or fssm.no_files != last_fssm.no_files:
-                    changing =True
-            if npoints <2: changing = True    # if we have one or less points in the 120 days then assume changing.
+                    changing = True
+            if npoints < 2:
+                changing = True    # if we have one or less points in the 120 days then assume changing.
    
-            if len(fssms) < 10 and not f.sd_backup: print_msg(f, "Newish and not marked for backup.")
+            if len(fssms) < 10 and not f.sd_backup:
+                print_msg(f, "Newish and not marked for backup.")
         else: 
             print_msg(f, "Not measured yet")
+            continue
 
-        if too_many_files: print_msg(f, "Too Many files")
-        if too_big: print_msg(f, "Too Big")
-        if over_alloc: print_msg(f, "Over allocation")
+        if too_many_files:
+            print_msg(f, "Too Many files")
+        if too_big:
+            print_msg(f, "Too Big")
+        if over_alloc:
+            print_msg(f, "Over allocation")
 
         if f.sd_backup:
-            backup_processed =  f.sd_backup_process_log()[-13:-5]
-            if backup_processed[:3] == "Not": print_msg(f, "Not processed for backup yet")
+            backup_processed = f.sd_backup_process_log()[-13:-5]
+            if backup_processed[:3] == "Not":
+                print_msg(f, "Not processed for backup yet")
             else:
                 date = datetime(int(backup_processed[:4]), int(backup_processed[4:6]), int(backup_processed[6:8]))
                 if today - date > timedelta(days=10):
                     print_msg(f, "Not backed up for over 10 days")        
     
+
 def print_msg(obj, msg):
+    """print a message about an object with a link to admin interface"""
     url = "http://cedadb.ceda.ac.uk/admin/cedainfoapp/"
     #print obj._meta
     #print dir(obj._meta)
@@ -85,7 +95,8 @@ def print_msg(obj, msg):
     url += "%s/%s" % (obj_type, obj.pk)
     print "%s [%s] %s" % (url, msg, obj)
 
-def run ():
+
+def run():
     fileset_problems()
     audit_problems()
     partition_problems()    
