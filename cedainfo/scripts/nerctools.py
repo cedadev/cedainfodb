166a1723177d71a85d1c51185542db4facf7d1ef
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2016-09-14 11:09:19 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@45901 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/scripts/nerctools.py
@@ -0,0 +1,70 @@
+
+#
+# This script is intended to be run via the 'runscript' option of manage.py:
+#
+#    python manage.py runscript <script-name-without-py-extension>
+#
+import sys
+import os
+import struct
+
+from django.core.management import setup_environ
+from django.conf import settings
+
+sys.path.append('/pyEnv/ve/cedainfo/cedainfo')
+
+import settings as dbsettings
+import tempfile
+import os
+import subprocess
+
+setup_environ(dbsettings)
+
+
+
+
+from udbadmin.models import *
+from django.db.models import Q
+
+DATASETID = "jasmin-login"
+
+def run():
+     
+    udjs = Datasetjoin.objects.filter(Q (datasetid='jasmin-login') | Q(datasetid='cems-login')).filter(removed=0)
+    
+    
+    for udj in udjs:
+    
+        print udj.userkey.userkey
+	
+        if udj.userkey.userkey == 1:
+            print 'AAAAAAA'
+            continue
+	    	    
+        nerctools = Datasetjoin.objects.filter(userkey=udj.userkey).filter(datasetid='nerctools')
+	
+	if udj.userkey == 1:
+            print 'ZZZ', udj.userkey
+	    
+	if len(nerctools) <= 0:
+	    print 'Add nerctools to ', udj.userkey
+
+        else:
+	     print 'already got nerctools'    
+	
+	    
+	      
+	    
+	    
+#    accountids = []
+#    
+#    for udj in udjs:
+#       accountids.append(udj.userkey.accountid)
+#       
+#       
+#    accountids.sort()
+#    
+#    for accountid in accountids:
+#        print accountid   
+
+
