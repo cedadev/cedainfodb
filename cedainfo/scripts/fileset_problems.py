761a8f0db19e524ebe62edc3f9c2a9ef2f93a920
spepler <spepler@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2016-09-15 11:20:28 +0000

    New problems email script using model methods.
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@45977 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/scripts/fileset_problems.py
+++ cedainfo/scripts/fileset_problems.py
@@ -100,7 +100,8 @@ def run():
     fs_msgs = FileSet.problems()
     audit_msgs = Audit.problems()
     part_msgs = Partition.problems()
-
+    print "http://cedadb.ceda.ac.uk/problems"
+    print
     print "%s Fileset problems" % len(fs_msgs)
     print "%s Partition problems" % len(part_msgs)
     print "%s Audit problems" % len(audit_msgs)
