ae0a3f1b5d4e938f2d64385d51dc287afe299f5f
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2016-12-06 14:08:02 +0000

    Syncing with version currently deployed
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@46702 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/findpandup.py
+++ cedainfo/findpandup.py
@@ -20,30 +20,33 @@ import filecmp
 from cedainfoapp.models import *
 
 
-            
-
-
 if __name__=="__main__":
     
     # for each pan partition...
-    pans = os.listdir('/datacentre/archvol')
+    pans = []
+    archvols = ('/datacentre/archvol', '/datacentre/archvol2')
+    for a in archvols:
+        pan_dirs = os.listdir(a)
+        for p in pan_dirs:
+            pans.append(os.path.join(a, p))
+
     for p in pans:
         #print p
         # for each (potential) spotname
-        archivename = os.path.join('/datacentre/archvol',  p, 'archive')
+        archivename = os.path.join(p, 'archive')
         if not os.path.exists(archivename): continue
         spots = os.listdir(archivename)
         for spotname in spots:
             filesets = FileSet.objects.filter(storage_pot=spotname, storage_pot_type = 'archive')
             pan_spotpath = os.path.join( archivename, spotname)
             if len(filesets)==0: 
-                print "** %s no matching fileset- why is it here - remove?" % pan_spotpath
+                print "** %s no matching fileset- why is it here - remove?\n" % pan_spotpath
                 continue
             if len(filesets)>1:
                 #print "** %s more than 1 matching fileset- skip" %spotname
                 continue
             fileset = filesets[0]
-            current_spotpath = fileset. storage_path()
+            current_spotpath = fileset.storage_path()
             if fileset.partition.mountpoint[0:12] != '/datacentre/':
                 #print "** current storage is not panasas - %s" % current_spotpath
                 continue
@@ -51,7 +54,9 @@ if __name__=="__main__":
                 #print "** %s pan spot and current spot are the same - skip" %spotname
                 continue
 
-            print "------- %s redundent? (current spot %s)" % (pan_spotpath,current_spotpath )  
+            print "------- %s redundent? (current spot %s)" % (pan_spotpath, current_spotpath)
+            os.system('ls -l %s' % fileset.logical_path)
+            print  
 
 
     
