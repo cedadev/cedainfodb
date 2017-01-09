34a78869efa895830ee7fd8a1fd1f104ee16ea47
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2017-01-09 10:43:45 +0000

    Added more users to ldap exclude lists
    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@46858 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

--- cedainfo/udbadmin/LDAP.py
+++ cedainfo/udbadmin/LDAP.py
@@ -6,7 +6,8 @@ from operator import itemgetter
 
 from django.conf import settings
 
-EXCLUDE_USERS = ['aharwood', 'mpryor', 'gparton', 'gpp02']
+EXCLUDE_USERS = ['aharwood', 'mpryor', 'gparton', 'gpp02', 'mpritcha', 'wtucker', 'rsmith013']
+
 #EXCLUDE_USERS = []
 
 GROUP_BASE  = "ou=ceda,ou=Groups,o=hpc,dc=rl,dc=ac,dc=uk"
