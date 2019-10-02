b55293842b8a4aa92e2d1d80477f5bc31958695c
aharwood <aharwood@a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40>
2019-10-02 15:15:26 +0000

    git-svn-id: http://team.ceda.ac.uk/svn/ceda/ceda_software/cedainfodb/django/trunk@48921 a6d0c4e4-8e0a-0410-98ec-c2d9f1a29e40

---
+++ cedainfo/cedainfoapp/list_docs.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+
+sys.path.append('/usr/local/cedainfodb/releases/current/cedainfo')
+
+
+from django.core.management import setup_environ
+from django.conf import settings
+import settings as dbsettings
+import requests
+
+setup_environ(dbsettings)
+
+from cedainfoapp.models import *
+
+import helpscoutdocs
+
+def list_duplicates(seq):
+  seen = set()
+  seen_add = seen.add
+  seen_twice = set( x for x in seq if x in seen or seen_add(x) )
+  return list( seen_twice )
+  
+  
+services = NewService.objects.all()
+
+urls = []
+
+for service in services:
+    if service.documentation:
+        urls.append(service.documentation)
+
+
+duplicate_docs = list_duplicates(urls)
+
+duplicates = []
+
+
+for d in duplicate_docs:
+    rec = {}
+    rec["doc"] = d
+    rec["services"] = []
+    
+    res = NewService.objects.filter(documentation=d)
+    
+    for r in res:
+        rec["services"].append(r)
+
+    duplicates.append(rec)
+    
+
+for d in duplicates:
+    print d["doc"]
+    
+    for s in d["services"]:
+        print s.name
---
+++ cedainfo/cedainfoapp/list_helpscout_categories.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+
+sys.path.append('/usr/local/cedainfodb/releases/current/cedainfo')
+
+
+from django.core.management import setup_environ
+from django.conf import settings
+import settings as dbsettings
+import requests
+
+setup_environ(dbsettings)
+
+from cedainfoapp.models import *
+
+
+
+
+
+#articles = _download_helpscout_service_articles ('59b25ba1042863033a1caf8f')
+AUTH = ('47ff11d6d6207a05189271b8805a8b888533a49a', 'X')
+
+list_url = 'https://docsapi.helpscout.net/v1/collections/%s/categories' % '59b25ba1042863033a1caf8f'
+r = requests.get(list_url, auth=AUTH)
+
+ncategories = len(r.json()["categories"]["items"])
+
+
+for n in range(0, ncategories):
+
+    print r.json()["categories"]["items"][n]["name"], r.json()["categories"]["items"][n]["id"]
+
+sys.exit()
+
+
+
