e6998bdbce781f260f2103304d3ba0e6228b875f
Andrew Harwood <andrew.harwood@stfc.ac.uk>
2019-12-05 09:32:09 +0000

    Update

--- cedainfo/scripts/vm_check.py
+++ cedainfo/scripts/vm_check.py
@@ -1,3 +1,6 @@
+#
+# Compare vm information in cedainfodb against ansible 'fact' information
+#
 import json
 import os
   
@@ -30,23 +33,50 @@ def run():
               p = data['ansible_facts']
 
               hostname = p['ansible_nodename']
+#
+#  Add 'centos6, centos7 to list. Remove rhel5
+#
+              os_string = 'unknown'
 
-              cpu_actual = int(p['ansible_processor_vcpus'])
-              cpu_requested = cpu_key[vm.cpu_required]
+              if p['ansible_distribution'] == 'RedHat':
+                  os_string = 'rhel' + p['ansible_distribution_major_version']
+              if p['ansible_distribution'] == 'CentOS':
+				  os_string = 'centos' + p['ansible_distribution_major_version']
 
-              mem = p['facter_memorysize_mb']
-              mem_actual = int(round(float(mem)/1000))
-              mem_requested = memory_key[vm.memory_required]
+              if os_string != vm.os_required:
+                  print hostname, vm.os_required, os_string
+				  print "update cedainfoapp_vm set os_required='%s' where name='%'" % (os_string, vn.name)
 
+	   #              if p['ansible_distribution'] == 'RedHat' and p['ansible_distribution_major_version'] == '6':
+#                  if vm.os_required != 'rhel6':
+#                     print hostname, vm.os_required, p['ansible_distribution'], p['ansible_distribution_major_version']
+
+#              if p['ansible_distribution'] == 'RedHat' and p['ansible_distribution_major_version'] == '7':
+#                  if vm.os_required != 'rhel7':
+#                     print hostname, vm.os_required, p['ansible_distribution'], p['ansible_distribution_major_version']
+
+#              if p['ansible_distribution'] == 'CentOS' and p['ansible_distribution_major_version'] == '6':
+#				  if vm.os_required != 'centos6':
+#
+#				  print hostname, p['ansible_distribution_major_version']
+
+
+              # cpu_actual = int(p['ansible_processor_vcpus'])
+              # cpu_requested = cpu_key[vm.cpu_required]
+			  #
               # if cpu_requested != cpu_actual:
               #   new_cpu_string = cpu_key.keys()[cpu_key.values().index(cpu_actual)]
               #   print hostname, "cpu:", cpu_requested, "->", cpu_actual, "(%s)" % new_cpu_string
               #   print "update cedainfoapp_vm set cpu_required='%s' where name = '%s'" % (new_cpu_string, vm.name)
-
-              if mem_requested != mem_actual:
-                 new_mem_string = memory_key.keys()[memory_key.values().index(mem_actual)]
-                 print hostname, mem_requested, mem_actual, new_mem_string
-                 print "update cedainfoapp_vm set memory_required='%s' where name = '%s'" % (new_mem_string, vm.name)
+			  #
+              # mem = p['facter_memorysize_mb']
+              # mem_actual = int(round(float(mem)/1000))
+              # mem_requested = memory_key[vm.memory_required]
+			  #
+              # if mem_requested != mem_actual:
+              #    new_mem_string = memory_key.keys()[memory_key.values().index(mem_actual)]
+              #    print hostname, mem_requested, mem_actual, new_mem_string
+              #    print "update cedainfoapp_vm set memory_required='%s' where name = '%s'" % (new_mem_string, vm.name)
           except:
              print ('Error processing %s' % vm.name)
        else:
