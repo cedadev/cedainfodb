e554b8a1acb298aa0a6fbfa6319f3e050c698469
root <root@archman3.ceda.ac.uk>
2025-09-17 11:00:40 +0100

    Updated recognition of lets encrypt certificates

--- cedainfo/scripts/check_domains.py
+++ cedainfo/scripts/check_domains.py
@@ -9,7 +9,7 @@ import sys
 
 from cedainfoapp.models import *
 import datetime
-import OpenSSL
+#import OpenSSL
 import ssl
 
 
@@ -50,8 +50,8 @@ def get_certificate_details (domain):
         expireDate = datetime.datetime.strptime(certificate["notAfter"], "%b %d %H:%M:%S %Y %Z")
         expireDate = datetime.datetime.strftime(expireDate, '%d-%b-%Y')
         issuer_full = certificate["issuer"][2][0][1]
-        
-        if issuer_full == 'R10' or issuer_full == 'R11' or issuer_full == 'E5' or issuer_full == 'E6':
+      #  print ('Issuer: ', issuer_full)        
+        if issuer_full == 'R10' or issuer_full == 'R11' or issuer_full == 'R12' or issuer_full == 'E5' or issuer_full == 'E6':
             issuer = "LetsEncrypt"
         else:
             issuer = "Other"
